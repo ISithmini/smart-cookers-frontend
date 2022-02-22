4c92bbf505640b39491b5df6e65b19460d7a5519
Imashaaaa <imashasithmini2@gmail.com>
2022-02-22 11:06:52 +0530

    route lock modified

--- src/Helpers/RouteLock.tsx
+++ src/Helpers/RouteLock.tsx
@@ -12,9 +12,18 @@ const RoutLock: React.FunctionComponent<RoutLockProps> = ({path , redirect}) => 
     const { user} = useContext(AuthContext);
 
     let isAccessGranted = false;
-    if(user.role=='admin' || user.role=='sales' || user.role=='inventory'){
-        isAccessGranted= true;
+    if(user) {
+        if(user.role=='admin' || user.role=='sales' || user.role=='inventory'){
+            isAccessGranted= true;
+        }else{
+            isAccessGranted = false
+        }
+
+    }else{
+        isAccessGranted = false
+
     }
+    
     
     if(isAccessGranted) {
         return path;
